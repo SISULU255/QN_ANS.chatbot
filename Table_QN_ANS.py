3ceb848e246de2ba67b775204fb9cfcb447dc1c8
SISULU255 <97722123+SISULU255@users.noreply.github.com>
2022-11-01 15:59:49 +0300

    Update Table_QN_ANS.py

--- Table_QN_ANS.py
+++ Table_QN_ANS.py
@@ -1,6 +1,8 @@
 from transformers import pipeline
 import pandas as pd
 import streamlit as st
+#os.system ("pip install git+https://github.com/explosion/spacy-transformers")
+os.system("pip install git+https://github.com/rusty1s/pytorch_scatter")
 # prepare table + question
 st.title('''VIRTUAL ASSISTANT''')
 data = {"Institute": ["Kiitec"], "CONTACT INFO of kiitec": ["Kiitec contact info\nP.O.Box 3172 Arusha, Tanzania.\nPhone: +255 27 250 4384\nMobile: +255 757 845 118\nEmail: info@kiitec.ac.tz"], "Location": ["is located in Moshono, Arusha next to Masai Camp."], "Description of kiitec": [" is a technical institution registered by NACTE (REG/EOS/027) based in Moshono, Arusha next to Masai Camp. "], "Registration": ["Kiitec institution is registered by NACTE (REG/EOS/027)"], "Courses or programmes": ["Electronics & Telecommunication Engineering ,Industrial Automation\nComputer Engineering and Networking\nRenewable Energies, Environmental Impact\nFuture training programs or courses in development: Biomedical\nAvionics."], "Fee amount for diploma course": ["for first semester in diploma amount is 695,000 and  625,000 \n in second semister  can be paid in two installment "], "Fees payment info": ["The fees should be paid through the BANK of ABSA\n and Account No is: 002-4001687\nAccount Name: KIITEC Ltd "], "Uniform information ": ["The dressing code of kiitec institution are dark blue trouser, light blue shirts for men and dark blue skirts, light blue shirts for women "]}
